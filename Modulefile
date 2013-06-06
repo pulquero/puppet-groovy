ee144c0e1d27a42c01340e77a925192010784944
Spencer Herzberg <spencer.herzberg@gmail.com>
2013-06-05 20:44:08 -0500

    initial commit

---
+++ Modulefile
@@ -0,0 +1,10 @@
+name    'puppet-groovy'
+version '0.0.1'
+source 'https://github.com/sherzberg/puppet-groovy'
+author 'Spencer Herzberg'
+license 'Apache License, Version 2.0'
+summary 'Puppet module for installing Groovy from the official distribution.'
+description 'This module installs Groovy from the offical project site.'
+project_page 'https://github.com/sherzberg/puppet-groovy'
+
+dependency 'smarchive/archive', '0.1.1'
---
+++ manifests/init.pp
@@ -0,0 +1,71 @@
+# == Class: groovy
+#
+# Supported operating systems are:
+#   - Ubuntu Linux
+#   - Fedora Linux
+#   - Debian Linux
+#
+# === Authors
+#
+# Spencer Herzberg <spencer.herzberg@gmail.com>
+#
+class groovy (
+  $version  = 'UNSET',
+  $base_url = 'UNSET',
+  $url      = 'UNSET',
+  $target   = 'UNSET',
+  $timeout  = 360,
+) {
+
+  $version_real = $version ? {
+    'UNSET' => '2.1.4',
+    default => $version,
+  }
+
+  $base_url_real = $base_url ? {
+    'UNSET' => "http://dist.groovy.codehaus.org/distributions",
+    default => $base_url,
+  }
+
+  $url_real = $url ? {
+    'UNSET' => "${base_url_real}/groovy-binary-${version_real}.zip",
+    default => $url,
+  }
+
+  $target_real = $target ? {
+    'UNSET' => "/opt/groovy-${version_real}",
+    default => $target,
+  }
+
+  Exec {
+    path  => [
+      '/usr/local/sbin', '/usr/local/bin',
+      '/usr/sbin', '/usr/bin', '/sbin', '/bin',
+    ],
+    user  => 'root',
+    group => 'root',
+  }
+
+  archive { "groovy-binary-${version_real}.zip":
+    ensure     => present,
+    url        => $url_real,
+    checksum   => false,
+    src_target => '/var/tmp',
+    target     => '/opt',
+    extension  => 'zip',
+    timeout    => $timeout,
+  }
+
+  file { $target_real:
+    ensure  => link,
+    target  => "/opt/groovy-${version_real}",
+    require => Archive["groovy-binary-${version_real}.zip"],
+  }
+
+  file { '/etc/profile.d/groovy.sh':
+    ensure  => file,
+    mode    => '0644',
+    content => template("${module_name}/groovy.sh.erb"),
+  }
+
+}
